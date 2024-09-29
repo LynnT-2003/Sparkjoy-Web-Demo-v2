29c39a741fa1934b23e6ec6b029bf8197f41eba8
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-09-30 00:17:07 +0700

    Updated UI.

--- components/sections/HistoryImagesSection.tsx
+++ components/sections/HistoryImagesSection.tsx
@@ -1,5 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 
 const HistoryImagesSection = () => {
   const [images, setImages] = useState<string[]>([]);
@@ -36,7 +38,7 @@ const HistoryImagesSection = () => {
 
   return (
     <div className="px-24">
-      <h2 className="text-xl font-bold mt-12 mb-4 ">
+      <h2 className="text-xl font-bold mt-12 mb-6 pl-2">
         History of Generated Images
       </h2>
 
@@ -49,15 +51,34 @@ const HistoryImagesSection = () => {
           <p className="ml-4">Loading...</p>
         </div>
       ) : images.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pb-24 gap-6">
           {images.map((image, index) => (
-            <div key={index} className="p-2 lg-rounded">
-              <img
-                src={`data:image/png;base64,${image}`} // Assuming images are base64-encoded
-                alt={`Generated Image ${index}`}
-                className="w-full h-auto rounded-lg"
-              />
-            </div>
+            <CardContainer className="inter-var">
+              <CardBody className="relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-full h-full rounded-xl p-6 px-2 border  ">
+                {/* <CardItem
+                  translateZ="50"
+                  className="text-xl font-bold text-white dark:text-white"
+                >
+                  Make things float in air
+                </CardItem>
+                <CardItem
+                  as="p"
+                  translateZ="60"
+                  className="text-white text-sm max-w-sm mt-2 dark:text-neutral-300"
+                >
+                  Hover over this card to unleash the power of CSS perspective
+                </CardItem> */}
+                <CardItem translateZ="100" className="w-full mt-0">
+                  <Image
+                    src={`data:image/png;base64,${image}`} // Use base64 string
+                    height={1000}
+                    width={1000}
+                    className="object-cover rounded-xl group-hover/card:shadow-xl"
+                    alt="thumbnail"
+                  />
+                </CardItem>
+              </CardBody>
+            </CardContainer>
           ))}
         </div>
       ) : (
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["images.unsplash.com"], // Add this line to allow images from unsplash.com
+  },
+};
 
 export default nextConfig;
