708b83e00e9707a4e5ded5382ebf8d92f0437662
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-11-18 06:46:36 +0700

    Update on image upload success screen.

--- app/ImageUpload/upload-success/page.tsx
+++ app/ImageUpload/upload-success/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon, CheckCircle2Icon } from "lucide-react";
 
 const UploadSuccessScreen = () => {
   const [base64String, setBase64String] = useState<string | null>(null);
@@ -17,9 +20,45 @@ const UploadSuccessScreen = () => {
   }
 
   return (
-    <div>
-      <h1>Upload Success</h1>
-      <img src={base64String} alt="Uploaded File" />
+    <div className="flex flex-col justify-center w-[100dvw]">
+      <h1 className="mx-12 text-center pt-[1.7rem] text-2xl font-semibold font-sans motion-preset-slide-right">
+        Image Uploaded Successfully!
+      </h1>
+      <div className="relative mx-12 aspect-square mt-5 rounded-lg ">
+        <Image
+          src={base64String}
+          alt="Uploaded Image"
+          layout="fill"
+          objectFit="cover"
+          className="rounded-lg z-0 opacity-25 motion-preset-expand motion-duration-300"
+        />
+        <div className="w-[80%] h-[80%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 flex flex-col items-center justify-center">
+          <CheckCircle2Icon
+            className="w-[40%] h-[40%] text-green-500 motion-preset-pop motion-duration-1000"
+            style={{
+              transformOrigin: "center",
+            }}
+          />
+          <h1 className="text-center pt-4 text-md font-semibold font-sans opacity-100 motion-preset-expand motion-duration-300">
+            Your Image is now ready
+          </h1>
+          <h1 className="text-center pt-0 text-md font-semibold font-sans opacity-100 motion-preset-expand motion-duration-300">
+            to be generated.
+          </h1>
+        </div>
+      </div>
+
+      <div className="flex mx-12 items-center justify-between space-x-4">
+        <Button className="mt-7 w-full" variant="default">
+          Go Back
+        </Button>
+        <Button
+          className="mt-7 w-full bg-gradient-to-r from-pink-900 to-purple-900 "
+          variant="secondary"
+        >
+          Generate <ArrowRightIcon className="w-4 h-5 ml-2" />
+        </Button>
+      </div>
     </div>
   );
 };
