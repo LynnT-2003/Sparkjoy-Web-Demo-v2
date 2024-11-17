74eebaa411c1f7d690e5aa8f1ef34f6c6c88a579
Lynn Thit Nyi Nyi <lnyinyi22@gmail.com>
2024-11-18 04:47:51 +0700

    Update: Fix localStorage referenceError on Deployment.

--- app/ImageUpload/upload-success/page.tsx
+++ app/ImageUpload/upload-success/page.tsx
@@ -1,6 +1,16 @@
 "use client";
+import { useState, useEffect } from "react";
+
 const UploadSuccessScreen = () => {
-  const base64String = localStorage.getItem("uploadedFile");
+  const [base64String, setBase64String] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // Access localStorage safely only on the client side
+      const storedFile = localStorage.getItem("uploadedFile");
+      setBase64String(storedFile);
+    }
+  }, []);
 
   if (!base64String) {
     return <div>No file found.</div>;
